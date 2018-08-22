7524c5f19d8b6df5d8107717b57e04e18efed8b5
Ali GOREN <goren.ali@yandex.com>
2018-08-22 21:31:50 +0300

    Added UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $users = User::paginate(10);
+
+        return view('users/users', ['users' => $users]);
+    }
+}
