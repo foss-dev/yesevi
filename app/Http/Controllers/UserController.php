86d8d14f6cb19b11665846577ae3696bdd80bd3b
Ali GOREN <goren.ali@yandex.com>
2018-08-23 14:19:39 +0300

    Added hash class to UserController Added store function to register new user Added fillable field for is_admin

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use App\User;
 
 class UserController extends Controller
@@ -17,5 +18,63 @@ class UserController extends Controller
         $users = User::paginate(10);
 
         return view('users/users', ['users' => $users]);
+    }
+
+    public function store(Request $req)
+    {
+        $status = [
+            'error' => false,
+            'message' => ''
+        ];
+        $keys = $req->all();
+
+        $username = $keys['username'];
+        $email = $keys['email'];
+        $password = $keys['password'];
+        $confirmPassword = $keys['confirmPassword'];
+        $isAdmin = $keys['isAdmin'];
+
+        $userCheck = User::where('email', $email)->first();
+
+        if($userCheck)
+        {
+            $status['error'] = true;
+            $status['message'] = "This e-mail registered before";
+        }
+
+        if(!$username)
+        {
+            $status['error'] = true;
+            $status['message'] = "The username can't be blank";
+        }
+
+        if(!$email)
+        {
+            $status['error'] = true;
+            $status['message'] = "The e-mail can't be blank";
+        }
+
+        if($password !== $confirmPassword && !(count($password) >=6 && count($confirmPassword) >= 6))
+        {
+            $status['error'] = true;
+            $status['message'] = "Passwords must be same and minimum lenght must be 6 characters.";
+        }
+
+        if(!$status['error'])
+        {
+            $user = User::create([
+                'name' => $username,
+                'email' => $email,
+                'password' => Hash::make($password),
+                'is_admin' => $isAdmin
+            ]);
+
+            if($user) {
+                $status['message'] = 'User added successfully!';
+
+            }
+        }
+
+        return response()->json($status);
     }
 }
