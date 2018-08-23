0e7c77084f44d81345429befa7c5ee543a13472f
Ali GOREN <goren.ali@yandex.com>
2018-08-23 14:22:41 +0300

    Updated user register modal Added alert area

--- resources/views/modals/user.blade.php
+++ resources/views/modals/user.blade.php
@@ -9,9 +9,9 @@
             <div class="columns is-multiline">
                 <div class="column is-6">
                     <div class="field">
-                        <label for="username" class="label">{{ __('messages.username')  }}</label>
+                        <label for="username" class="label">{{ __('messages.name')  }}</label>
                         <div class="control has-icons-left has-icons-right">
-                            <input id="username" class="input is-info" type="text" name="username" placeholder="{{ __('messages.username')  }}" value="" required>
+                            <input id="username" class="input is-info" type="text" name="username" placeholder="{{ __('messages.name')  }}" required data-disable-auto-complete="true" autocomplete="off" />
                             <span class="icon is-small is-left"><i class="fas fa-user"></i></span>
                         </div>
                     </div>
@@ -20,15 +20,54 @@
                     <div class="field">
                         <label for="email" class="label">{{ __('messages.email')  }}</label>
                         <div class="control has-icons-left has-icons-right">
-                            <input id="email" class="input is-info" type="email" name="email" placeholder="{{ __('messages.email')  }}" value="" required>
+                            <input id="email" class="input is-info" type="email" name="email" placeholder="{{ __('messages.email')  }}"  required data-disable-auto-complete="true" autocomplete="off" />
                             <span class="icon is-small is-left"><i class="fas fa-at"></i></span>
                         </div>
+                    </div>
+                </div>
+                <div class="column is-6">
+                    <div class="field">
+                        <label for="password" class="label">{{ __('messages.password')  }}</label>
+                        <div class="control has-icons-left has-icons-right">
+                            <input id="password" class="input is-info" type="password" name="password" placeholder="{{ __('messages.password')  }}" required data-disable-auto-complete="true" autocomplete="off"/>
+                            <span class="icon is-small is-left"><i class="fas fa-key"></i></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="column is-6">
+                    <div class="field">
+                        <label for="confirm-password" class="label">{{ __('messages.password')  }}</label>
+                        <div class="control has-icons-left has-icons-right">
+                            <input id="confirm-password" class="input is-info" type="password" name="confirm-password" placeholder="{{ __('messages.confirm-password')  }}" required data-disable-auto-complete="true" autocomplete="off"/>
+                            <span class="icon is-small is-left"><i class="fas fa-key"></i></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="column is-6">
+                    <div class="field">
+                        <label class="checkbox">
+                            <input type="checkbox" name="is-admin">
+                            <strong>Admin</strong>
+                        </label>
+                    </div>
+                </div>
+                <div class="column is-12 user-modal-alert is-hidden ">
+                    <div class="field">
+                        <article class="message is-danger ">
+                            <div class="message-header">
+                                <p>Link</p>
+                                <button class="delete" aria-label="delete"></button>
+                            </div>
+                            <div class="message-body user-form-alert">
+
+                            </div>
+                        </article>
                     </div>
                 </div>
             </div>
         </section>
         <footer class="modal-card-foot">
-            <button class="button is-success">Save User</button>
+            <button class="button is-success save-user">Save User</button>
             <button class="button" data-bulma-modal="close">Cancel</button>
         </footer>
     </div>
